98959c90d87e182c72748eb9a5bd3d166fd26113
katungi <dankatdennis@gmail.com>
2020-08-05 14:32:45 +0300

    installing tailwind and replacing normal css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import HelloWorld from './components/HelloWorld';
-import CounterExample from './components/CounterExample';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     // react must always return a single element
     <div>
-     {/* <HelloWorld name="God"/> */}
+     <Header/>
 
-     <CounterExample/>
     </div>
   );
 }
