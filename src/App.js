fa6ad3735c098c141d9bc6577cf19631c9679c38
katungi <dankatdennis@gmail.com>
2020-08-05 14:14:54 +0300

    learning react hoot (useState)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import HelloWorld from './components/HelloWorld';
+import CounterExample from './components/CounterExample';
 import './App.css';
 
 function App() {
   return (
     // react must always return a single element
     <div>
-     <HelloWorld name="God"/>
+     {/* <HelloWorld name="God"/> */}
+
+     <CounterExample/>
     </div>
   );
 }
