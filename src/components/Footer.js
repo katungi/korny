e21d6e7250fe3e7cb6d6bef225a5f2bf0ece8473
katungi <dankatdennis@gmail.com>
2020-08-06 15:41:16 +0300

    destructured the app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(){
     return (
-        <footer className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
+        <footer className="bg-gray-200 text-center text-xs p-3 relative bottom-0 w-full">
             &copy; Copyright Daniel Dennis 2020
         </footer>
     )
--- src/views/Product.js
+++ src/views/Product.js
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// import { useParams} from 'react-router-dom';
 
 function Product() {
-  const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/";
-  const [product, setProduct] = useState(null);
-
-  let content = null;
+//   const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/";
+    // const { id }= useParams();
+  const url = `https://5f2b896fffc88500167b7fe6.mockapi.io/api/products/`;
+  const [products, setProducts] = useState(null);
+    console.log(url);
+//   let content = null;
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setProduct(response.data[0]);
-    });
+    const getProducts = async () => {
+      try {
+        const { data } = await axios.get(url);
+        // console.log(data.images);
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
   }, [url]);
-  console.log(product);
-  if (product) {
-    content =
-        <div>
-          <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
-          <div>
-            {/* <img src={product.images[0].imageUrl} alt={product.name} /> */}
+
+  //   console.log(product);
+  return (
+    <>
+      {products ? (
+        products.map(({ id, name, price, description, images }) => (
+          <div key={id}>
+            <h1 className="text-2xl font-bold mb-3">{name}</h1>
+            <div>
+              {images ? (
+                <img src={images} alt={name} />
+              ) : (
+                  <img src="https://picsum.photos/200/300" alt="Ã°Å¸Å¡â‚¬" />
+                )}
+            </div>
+            <div className="font-bold text-xl mb-3">${price}</div>
+            <div>{description}</div>
           </div>
-          <div className="font-bold text-xl mb-3">${product.price}</div>
-          <div>{product.description}</div>
+        ))
+      ) : (
+        <div>
+          <h1>Error</h1>
         </div>
-  }
-  return <div><h1>Error</h1></div>;
+      )}
+    </>
+  );
 }
 export default Product;
