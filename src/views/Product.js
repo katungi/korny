4b159c994c47d85d8770d5eec9f41f90425aaf3a
katungi <dankatdennis@gmail.com>
2020-08-06 16:18:25 +0300

    added loader and more refactoring

--- src/views/Product.js
+++ src/views/Product.js
@@ -4,19 +4,20 @@ import Loader from '../components/Loader';
 import { useAxiosGet } from '../Hooks/HttpRequests'
 
 function Product(){
-    const { id } = useParams()
+    // const { id } = useParams()
     // Create your own Mock API: https://mockapi.io/
-    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`
+    const url = `https://5f2b896fffc88500167b7fe6.mockapi.io/api/products/`
     
     let product = useAxiosGet(url)
 
     let content = null
 
+    
     if(product.error){
         content = <div>
-            <div className="bg-blue-300 mb-2 p-3">
+            {/* <div className="bg-blue-300 mb-2 p-3">
                 If you see this error. Please remember to create your own <a href="https://mockapi.io/">mock API</a>.
-            </div>
+            </div> */}
             <div className="bg-red-300 p-3">
                 There was an error please refresh or try again later.
             </div>
@@ -27,7 +28,9 @@ function Product(){
         content = <Loader></Loader>
     }
 
+    
     if(product.data){
+        console.log(product.data.image)
         content = 
         <div>
             <h1 className="text-2xl font-bold mb-3">
@@ -35,7 +38,7 @@ function Product(){
             </h1>
             <div>
                 <img
-                    src={product.data.images[0].imageUrl}
+                    src={product.data.image}
                     alt={product.data.name}
                 />
             </div>
@@ -44,6 +47,7 @@ function Product(){
             </div>
             <div>
                 {product.data.description}
+                
             </div>
         </div>
     }
