fdeb4f4a1046814f1765e0af13ca19ae3101f599
katungi <dankatdennis@gmail.com>
2020-08-06 15:54:40 +0300

    last draw

--- src/views/Product.js
+++ src/views/Product.js
@@ -6,16 +6,25 @@ function Product() {
 //   const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/";
     // const { id }= useParams();
   const url = `https://5f2b896fffc88500167b7fe6.mockapi.io/api/products/`;
-  const [products, setProducts] = useState(null);
-    console.log(url);
+  const [products, setProducts] = useState({
+      loading: false,
+      data: null
+  });
 //   let content = null;
 
   useEffect(() => {
+      setProducts({
+          loading:true,
+          data:null
+      })
     const getProducts = async () => {
       try {
         const { data } = await axios.get(url);
         // console.log(data.images);
-        setProducts(data);
+        setProducts({
+            loading: false,
+            data: data
+        });
       } catch (error) {
         console.log(error);
       }
@@ -23,11 +32,11 @@ function Product() {
     getProducts();
   }, [url]);
 
-  //   console.log(product);
+  console.log(products.data);
   return (
     <>
       {products ? (
-        products.map(({ id, name, price, description, images }) => (
+        products.data.map(({ id, name, price, description, images }) => (
           <div key={id}>
             <h1 className="text-2xl font-bold mb-3">{name}</h1>
             <div>
